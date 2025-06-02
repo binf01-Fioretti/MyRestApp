ae6645bef1e25a6bf9e0afcc3f07371f7b209497
Daniele Fioretti <fiorettidaniele9@gmail.com>
2025-06-02 11:23:08 +0200

    aggiunte funzioni messaggioToast e aggiunto della WebView

--- app/src/main/java/com/example/myrestapp/MenuActivity.kt
+++ app/src/main/java/com/example/myrestapp/MenuActivity.kt
@@ -1,6 +1,10 @@
 package com.example.myrestapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,14 +21,5 @@ class MenuActivity : AppCompatActivity() {
             insets
         }
     }
-     override fun onClick(){
-        var btnClick = findViewById<Button>(R.id.buttonClick)
-         btnClick.setOnClickListener(View.OnClickListener {
-             messaggioToast("LOGIN EFFETTUATO!")
-     }
-}
-    private fun creaIntent(){
-        var Intent= Intent(this,WebWiewActivity::class.java)
-        startActivity(Intent)
-    }
+
 }
