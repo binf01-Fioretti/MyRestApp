0cd520b0d130a696915373c79c976217376fded0
Daniele Fioretti <fiorettidaniele9@gmail.com>
2025-06-03 12:24:08 +0200

    aggiunte funzioni onClickMenu() e messaggioToastMenu()

--- app/src/main/java/com/example/myrestapp/MenuActivity.kt
+++ app/src/main/java/com/example/myrestapp/MenuActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,5 +22,18 @@ class MenuActivity : AppCompatActivity() {
             insets
         }
     }
-
-}
+    fun onClickMenu(){
+        var btnMenu = findViewById<ImageButton>(R.id.imageButton)
+        btnMenu.setOnClickListener(View.OnClickListener {
+            messaggioToastMenu("LOGIN EFFETTUATO!")
+        })
+    }
+    fun messaggioToastMenu(msg: String){
+        var mioToast = Toast.makeText(
+            this,
+            msg,
+            Toast.LENGTH_SHORT
+        )
+        mioToast.show()
+    }
+    }
